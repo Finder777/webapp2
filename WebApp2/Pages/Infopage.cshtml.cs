bc231c8925709b10633803040ee0c44c1272666a
Finder777 <ed.ag.ramirez@hotmail.com>
2023-10-05 12:59:24 +0100

    Add project files.

---
+++ WebApp2/Pages/Infopage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp2.Pages
+{
+    public class InfopageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ WebApp2/Pages/Shopping.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp2.Pages
+{
+    public class ShoppingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
